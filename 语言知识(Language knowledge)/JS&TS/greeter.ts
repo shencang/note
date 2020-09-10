328053ed052d08dff26c8b2bc3120c87a60c4512
shencang <2630610009@qq.com>
2020-09-10 22:47:55 +0800

    ts

---
+++ 语言知识(Language knowledge)/JS&TS/greeter.ts
@@ -0,0 +1,36 @@
+// function greeter(persion){
+//     return "你好," + persion
+// }
+// let user = "Jane User"
+// document.body.innerHTML = greeter(user)
+
+// function greeter(persion:string){
+//     return "hello,"+persion
+// }
+// let user = "Jane User"
+// let user2 = [1,0,2]
+// document.body.innerHTML = greeter(user)
+
+// interface Person{
+//     firstName: string
+//     lastName: string
+// }
+
+// function greeter(persion:Person){
+//     return "hello,"+persion.firstName +" "+persion.lastName
+// }
+
+// let user = {
+//     firstName:"Jane",
+//     lastName: "user"
+// } 
+// document.body.innerHTML =greeter(user)
+
+
+// class Student{
+//     fullName: string
+//     constructor(public firstName, public middleInital,public lastName){
+//         this.fullName = firstName = firstName + " " +middleInital + " "+ lastName
+               
+//     }
+// }
